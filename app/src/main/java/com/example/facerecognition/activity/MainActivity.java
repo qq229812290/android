2941365723a9df6e1c69c91f49ab5ec14240eadb
xiaoyege521 <94595610@qq.com>
2019-11-07 10:40:28 +0800

    zhaoyan2

--- app/src/main/java/com/example/facerecognition/activity/MainActivity.java
+++ app/src/main/java/com/example/facerecognition/activity/MainActivity.java
@@ -24,7 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     public void turnToFaceCollection(View view) {
